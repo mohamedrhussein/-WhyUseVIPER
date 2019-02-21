44d7715fb203c6680bc44c869000c3b4a8dc206c
Mohamed Rabie <mohamed.rabie@pharosholding.com>
2019-02-21 16:11:14 +0200

    initial pure project files

--- WhyUseVIPER/AppDelegate.swift
+++ WhyUseVIPER/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //start github using viper desgin patterns
+        
         return true
     }
 
